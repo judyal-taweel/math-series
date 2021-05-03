c69ab16c5ad368de98d8208ab43bcec5c27d3fb1
judyal-taweel <judybataineh@gmail.com>
2021-05-03 21:33:56 +0300

    test math series fibonacci and lucas

--- math_series/series.py
+++ math_series/series.py
@@ -1,13 +1,27 @@
 def fibonacci(n):
-   return sum_series(n)
+    """
+    for number 0 will return 0
+    for number 1 will return 1
+    for another numbers will sum (n-1)+(n-2)
+    """
+    return sum_series(n)
 
 # print(fibonacci(7))
    
 def lucas(n):
+    """
+    for number 0 will return 2
+    for number 1 will return 1
+    for another numbers will sum (n-1)+(n-2)
+    """
     return sum_series(n,2,1)
 # print(lucas(7))
 
 def sum_series(n,x=0,y=1):
+    """
+    for didn't add number will use optional parameter x,y fibonacci(n)
+    when add 2,1 will run as lucas(n)
+    """
     if n >= 0:
       if n == 0:
         return x
